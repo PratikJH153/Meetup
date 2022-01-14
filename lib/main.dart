70a0dd19b127167323269617c14ec47ef4d3f335
AKAMasterMind404 <59548859+AKAMasterMind404@users.noreply.github.com>
2022-01-14 12:04:25 +0530

    dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meetupapp/view/home_page.dart';
 
 Future<void> main() async {
+
+  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
